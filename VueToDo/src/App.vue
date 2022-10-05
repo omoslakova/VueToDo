b44f0a9824dddf44be4f63c5ced8921fea0e80b9
omoslakova <o.moslakova@yandex.by>
2022-10-05 18:46:36 +0300

    added notes from test api and styled with grid

--- VueToDo/src/App.vue
+++ VueToDo/src/App.vue
@@ -1,35 +1,41 @@
 <template>
-  <h1>ToDo List</h1>
-  <div>
-    <input
-        v-model="newNoteName"
-        type="text"
-        placeholder="Add TO DO"
-        @keypress.enter="addNewNote"
-    />
-    <button @click="addNewNote">
-      ADD
-    </button>
-  </div>
-  <div>
-  <div>Task</div>
-  <div>Action</div>
-  <div>
-    <to-do
-        v-for="(toDo) in notes"
-        :key="toDo.id"
-        :to-do="toDo"
-        title="title"
-        @deleteNote="removeNote"
-    />
-  </div>
+  <div class="wrapper">
+    <h1 class="header"
+    >
+      ToDo List
+    </h1>
+    <div class="todo-input">
+      <input
+          v-model="newNoteName"
+          type="text"
+          placeholder="Add TO DO"
+          @keypress.enter="addNewNote"
+      />
+    </div>
+    <div class="add">
+      <button class="btn-add"
+              @click="addNewNote">
+        ADD
+      </button>
+    </div>
+    <div class="task">Task</div>
+    <div class="action">Action</div>
+    <div class="notes">
+      <to-do
+          v-for="(toDo) in notes"
+          :key="toDo.id"
+          :to-do="toDo"
+          title="title"
+          @deleteNote="removeNote"
+      />
+    </div>
   </div>
 </template>
 
-
 <script>
+
 import ToDo from './components/ToDo.vue'
-import { ref } from 'vue';
+import {ref, onMounted} from 'vue';
 
 export default {
   name: "App",
@@ -38,30 +44,14 @@ export default {
   },
   setup() {
     const newNoteName = ref('');
-    const notes = ref([
-      {
-        id: 1,
-        name: "note1",
-        checked: false
-      },
-      {
-        id: 2,
-        name: "note2",
-        checked: false
-      },
-      {
-        id: 3,
-        name: "note3",
-        checked: false
-      }
-    ]);
+    const notes = ref([]);
 
     const addNewNote = () => {
       // todo add validation
       // todo max value reduce
       const newNote = {
         id: Math.max(...notes.value.map(notes => notes.id)) + 1,
-        name: newNoteName.value,
+        todo: newNoteName.value,
         checked: false
       }
       notes.value.push(newNote);
@@ -69,9 +59,19 @@ export default {
     };
 
     const removeNote = (id) => {
-      const targetIndex = notes.value.findIndex( (el) => el.id === id)
+      const targetIndex = notes.value.findIndex((el) => el.id === id)
       notes.value.splice(targetIndex, 1)
     }
+
+    onMounted(() => {
+      fetch('https://dummyjson.com/todos')
+          .then((res) => {
+            return res.json();
+          })
+          .then((res) => {
+            notes.value = res.todos
+          })
+    })
 
     return {
       notes,
@@ -81,11 +81,60 @@ export default {
     };
   },
 };
+
 </script>
 
-
-
-
 <style scoped>
+
+.wrapper {
+  font-family: Georgia;
+  display: grid;
+  grid-template-columns: 30px 30px 160px 100px auto;
+  grid-template-rows: 80px 30px 30px auto;
+  text-align: center;
+}
+
+.header {
+  color: #2828b5;
+  grid-column: 3 / 3;
+  grid-row: 1 / 1;
+}
+
+.todo-input {
+  grid-column: 2 / 3;
+  grid-row: 2 / 2;
+  align-items: center;
+  outline: none;
+}
+
+.add {
+  grid-column: 4 / 4;
+  grid-row: 2 / 2;
+  justify-self: start;
+}
+
+.btn-add {
+  background-color: white;
+  color: #2828b5;
+  border: 2px solid #2828b5;
+  border-radius: 4px;
+}
+
+.task {
+  grid-column: 3 / 3;
+  grid-row: 3 / 3;
+  justify-self: start;
+}
+
+.action {
+  grid-column: 4 / 4;
+  grid-row: 3 / 3;
+  justify-self: start;
+}
+
+.notes {
+  grid-column: 1 / 4;
+  grid-row: 4 / 4;
+}
 
 </style>
--- VueToDo/src/components/ToDo.vue
+++ VueToDo/src/components/ToDo.vue
@@ -1,19 +1,24 @@
 <template>
   <div
-      class="todo"
+      class="todo-container"
       :class="{
           done: checked,
           }"
   >
+    <div>
     <input
       v-model="checked"
       type="checkbox"
   />
+    </div>
     <input
-       v-model="name"
+       v-model="todo"
+       class="todo-name"
        :disabled="checked"
     />
-    <button @click="emitDeleteNote"
+    <button
+        class="btn-delete"
+        @click="emitDeleteNote"
     >
       DELETE
     </button>
@@ -35,7 +40,7 @@ export default {
 
   setup(props, {emit}) {
 
-    const {checked, name} = toRefs(props.toDo)
+    const {checked, todo} = toRefs(props.toDo)
 
     const emitDeleteNote = () => {
       emit('deleteNote', props.toDo.id);
@@ -43,18 +48,42 @@ export default {
 
     return {
       checked,
-      name,
+      todo,
       emitDeleteNote,
-
     }
   }
 }
-
 </script>
 
-
 <style scoped>
+
+.todo-container {
+  display: grid;
+  grid-template-columns: 1fr 5fr 3fr ;
+  grid-template-columns: repeat(3, 1fr);
+  text-align: center;
+  gap: 10px;
+}
+
+input {
+  outline:none;
+}
+
+.btn-delete {
+  background-color: white;
+  color: black;
+  border: 2px solid black;
+  border-radius: 4px;
+}
+
+.todo-name {
+  font-size: 14px;
+  border: 0px;
+}
+
 .done {
   color: gray;
 }
+
+
 </style>
